6b9e12adfc1fa73f3b3627647314e1c8563ba3bd
Leandro Silva <leanddrorrs@outlook.com>
2025-01-20 08:43:58 -0300

    update file

--- src/services/credit_card_service.py
+++ src/services/credit_card_service.py
@@ -4,7 +4,6 @@ from azure.ai.documentintelligence.models import AnalyzeDocumentRequest
 from ultils.Config import Config
 
 def analize_credit_card(card_url):
- try:
   credential = AzureKeyCredential(Config.KEY)
   document_client = DocumentIntelligenceClient(Config.ENDPOINT, credential)
   card_info = document_client.begin_analyze_document("prebuilt-creditCard", AnalyzeDocumentRequest(url_source=card_url))
@@ -18,5 +17,3 @@ def analize_credit_card(card_url):
     "expiry_date": fields . get( 'ExpirationDate', ()).get('content'),
     "bank_name": fields .get( 'IssuingBank', ()).get('content'),
   }
- except Exception as ex:
-  return ex
