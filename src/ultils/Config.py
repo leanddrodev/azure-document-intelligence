4bcd901290cb326c0dabda3f47e7f6faee7f932d
Leandro Silva <leanddrorrs@outlook.com>
2025-01-20 06:29:29 -0300

    create Config.py

---
+++ src/ultils/Config.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+class Config:
+ ENDPOINT = os.getenv("DOCINTELL_ENDPOINT")
+ KEY = os.getenv("DOCINTELL_KEY1")
+ AZURE_STORAGE_CONNECTION_STRING = os.getenv("STORAGE_CONNECTION_STRING")
+ CONTAINER_NAME = os.getenv("STORAGE_CONTAINER_NAME")
